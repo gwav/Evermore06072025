6999c0b513d05276ff7a0cf7600c6259b43659f8
Gary <39293752+gwav@users.noreply.github.com>
2025-06-07 23:11:43 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -16,6 +16,20 @@ Hooks.once("init", () => {
         prepareData() {
             super.prepareData();
             console.log("Evermore RPG System | Preparing actor data...");
+            // Initialize default attributes if missing
+            if (!this.system.attributes) this.system.attributes = {};
+            const defaults = {
+                strength: { value: 8, max: 18 },
+                dexterity: { value: 8, max: 18 },
+                constitution: { value: 8, max: 18 },
+                intelligence: { value: 8, max: 18 },
+                wisdom: { value: 8, max: 18 },
+                charisma: { value: 8, max: 18 },
+                hp: { value: 10, max: 10 }
+            };
+            for (const [key, val] of Object.entries(defaults)) {
+                if (!this.system.attributes[key]) this.system.attributes[key] = foundry.utils.deepClone(val);
+            }
         }
     };
 
@@ -27,10 +41,23 @@ Hooks.once("init", () => {
         }
     }, { types: ["character"], makeDefault: true });
 
-    Actors.registerSheet("evermore", EvermoreActorSheet, { 
-        types: ["character", "npc"], 
-        makeDefault: true 
-    });
-
     console.log("Evermore RPG System | Initialization complete!");
+});
+
+// Ensure new actors get default attributes
+Hooks.on("preCreateActor", (actor, data, options, userId) => {
+    if (actor.type === "character") {
+        if (!data.system) data.system = {};
+        if (!data.system.attributes) {
+            data.system.attributes = {
+                strength: { value: 8, max: 18 },
+                dexterity: { value: 8, max: 18 },
+                constitution: { value: 8, max: 18 },
+                intelligence: { value: 8, max: 18 },
+                wisdom: { value: 8, max: 18 },
+                charisma: { value: 8, max: 18 },
+                hp: { value: 10, max: 10 }
+            };
+        }
+    }
 });
